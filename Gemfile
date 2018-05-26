2e6ef829f817774f52ebc8fe0f99f45283fcaae4
Jeff Kowalski <jeff.kowalski@gmail.com>
2018-05-26 13:22:26 -0700

    Add pool intent; use RestClient rather than Net::HTTP

--- Gemfile
+++ Gemfile
@@ -1,4 +1,12 @@
 source 'https://rubygems.org'
-gem 'alexa_rubykit', :git => 'https://github.com/damianFC/alexa-rubykit'
+
+gem 'alexa_rubykit'
 gem 'sinatra'
 gem 'humanize'
+gem 'json'
+gem 'rest-client'
+
+# for robe:
+gem 'pry'
+gem 'pry-doc', '>=0.6.0'
+gem 'method_source', '>=0.8.2'
--- jen.rb
+++ jen.rb
@@ -4,18 +4,12 @@
 # This Engine receives and responds to Amazon Echo's (Alexa) JSON requests.
 require 'sinatra'
 require 'sinatra/base'
-require 'json'
-require 'bundler/setup'
 require 'webrick'
 require 'webrick/https'
 require 'openssl'
 require 'alexa_rubykit'
-require 'date'
-require 'net/http'
-require 'uri'
-require 'open-uri'
-require 'yaml/store'
-require 'humanize'
+require 'json'
+
 
 def encodeURIcomponent str
   return URI.escape(str, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
@@ -23,9 +17,9 @@ end
 
 
 def make_org_entry heading, context, priority, date, body
-#  $logger.debug "TODO [#{priority}] #{heading} #{context}"
-#  $logger.debug "SCHEDULED: #{date}"
-#  $logger.debug "#{body}"
+  #  $logger.debug "TODO [#{priority}] #{heading} #{context}"
+  #  $logger.debug "SCHEDULED: #{date}"
+  #  $logger.debug "#{body}"
   title = encodeURIcomponent "[#{priority}] #{heading}  :#{context}:"
   body  = encodeURIcomponent "SCHEDULED: #{date}\n#{body}"
   uri = URI.parse("http://carbon:3333")
@@ -34,6 +28,7 @@ def make_org_entry heading, context, priority, date, body
   response = http.request(request)
   return response
 end
+
 
 module AlexaRubykit
   class Response
@@ -46,10 +41,12 @@ end
 
 
 class Float
+  require 'humanize'
+
   def fuzz unit
     int = itself.to_int
     frac = ((itself - int) * 100).to_int
-    prefix = case frac %25
+    prefix = case frac % 25
              when 0..3
                "about"
              when 4..12
@@ -93,7 +90,7 @@ class MyServer < Sinatra::Base
     # Check that it's a valid Alexa request
     request_json = JSON.parse(request.body.read.to_s)
     # Creates a new Request object with the request parameter.
-    request = AlexaRubykit.build_request(request_json)
+    alexa_request = AlexaRubykit.build_request(request_json)
 
     # We can capture Session details inside of request.
     # See session object for more information.
@@ -105,22 +102,20 @@ class MyServer < Sinatra::Base
     # p "user_defined    #{session.user_defined?}"
     # p "user            #{session.user}"
     # We need a response object to respond to the Alexa.
-    response = AlexaRubykit::Response.new
+    alexa_response = AlexaRubykit::Response.new
 
     # We can manipulate the request object.
     #
-    p "#{request.to_s}"
-    #p "#{request.request_id}"
+    p "#{alexa_request.to_s}"
+    #p "#{alexa_request.request_id}"
 
     end_session = true
 
-    # Response
-    # If it's a launch request
-    case request.type
+    case alexa_request.type
     when 'LAUNCH_REQUEST'
       # Process your Launch Request
       # Call your methods for your application here that process your Launch Request.
-      # p "type: #{request.type}"
+      # p "type: #{alexa_request.type}"
       response.add_speech("Jen is listening")
       response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Jen is listening!' } )
       end_session = false
@@ -128,25 +123,27 @@ class MyServer < Sinatra::Base
     when 'INTENT_REQUEST'
       speech = []
 
-      case request.name
+      case alexa_request.name
       when 'HelpIntent'
-        speech.unshift "You can ask me to remember something, or you can ask what are my jobs, or how much power are the solar panels producing."
+        speech.unshift "You can ask me to remember something, or you can ask what are my jobs, or how much power are the solar panels producing, or how warm is the pool."
 
       when 'TaskIntent'
+        require 'date'
+
         date = Time.now
-        if not request.slots['Date']['value'].nil?
+        if not alexa_request.slots['Date']['value'].nil?
           begin
-            date = Date.parse(request.slots['Date']['value'])
+            date = Date.parse(alexa_request.slots['Date']['value'])
           rescue
             date = Time.now
             speech.unshift "I didn't quite catch the date, so I put it down for today."
           end
         end
 
-        org_response = make_org_entry request.slots['Message']['value'], '@home', '#C',
-                                  "<#{date.strftime('%F %a')}>", ""
+        org_response = make_org_entry alexa_request.slots['Message']['value'], '@home', '#C',
+                                      "<#{date.strftime('%F %a')}>", ""
         if (org_response.code == '200')
-          speech.unshift "Sure, I'll remember to #{request.slots['Message']['value']} on #{date.strftime('%A')} <say-as interpret-as='date'>????#{date.strftime('%m%d')}</say-as>."
+          speech.unshift "Sure, I'll remember to #{alexa_request.slots['Message']['value']} on #{date.strftime('%A')} <say-as interpret-as='date'>????#{date.strftime('%m%d')}</say-as>."
         else
           speech.unshift "Sorry, make org entry responded with #{org_response.code} #{org_response.message}."
         end
@@ -158,20 +155,21 @@ class MyServer < Sinatra::Base
 
       when 'SolarIntent'
         begin
+          require 'yaml'
+          require 'rest-client'
+          require 'json'
+
           sunpower_credentials = YAML.load_file File.join(Dir.home, '.credentials', "sunpower.yaml")
-          api_base_url = "https://monitor.us.sunpower.com/CustomerPortal/"
+          api_base_url = "https://monitor.us.sunpower.com/CustomerPortal"
 
-          uri = URI.parse(api_base_url + "Auth/Auth.svc/Authenticate")
-          http = Net::HTTP.new uri.hostname, uri.port
-          http.use_ssl = true
-          auth_response = http.send_request 'POST', uri.path, sunpower_credentials.to_json, {'Content-Type' => 'application/json'}
-          decoded = JSON.parse(auth_response.body)
+          response = RestClient.post "#{api_base_url}/Auth/Auth.svc/Authenticate", sunpower_credentials.to_json, {'Content-Type' => 'application/json'}
+          authorization = JSON.parse response
 
-          current_power_response = open(api_base_url + "CurrentPower/CurrentPower.svc/GetCurrentPower?id=#{decoded['Payload']['TokenID']}").read
-          puts current_power_response
-          decoded = JSON.parse current_power_response
+          response = RestClient.get "#{api_base_url}/CurrentPower/CurrentPower.svc/GetCurrentPower?id=#{authorization['Payload']['TokenID']}"
+          puts response
+          power = JSON.parse response
 
-          production = decoded['Payload']['CurrentProduction'].to_f
+          production = power['Payload']['CurrentProduction'].to_f
           if production > 0
             if production < 1.0
               speech.unshift "The panels are producing #{(production*1000).fuzz('watt')}."
@@ -184,20 +182,66 @@ class MyServer < Sinatra::Base
         rescue Exception => e
           speech.unshift "There was a problem accessing sunpower.  The exception was #{e}"
         end
-        #puts buffer
+
+      when 'PoolIntent'
+        begin
+          require 'yaml'
+          require 'rest-client'
+          require 'json'
+
+          api_key = 'EOOEMOW4YR6QNB07'
+          credentials = YAML.load_file File.join(Dir.home, '.credentials', "iaqualink.yaml")
+
+          response = RestClient.post 'https://support.iaqualink.com/users/sign_in.json',
+                                     {api_key: api_key,
+                                      email: credentials[:username],
+                                      password: credentials[:password]}
+          session = JSON::parse response
+
+          response = RestClient.get 'https://iaqualink-api.realtime.io/v1/mobile/session.json',
+        	                    {params: {actionID: 'command',
+		                              command: 'get_home',
+		                              serial: credentials[:serial_number],
+		                              sessionID: session['session_id']}}
+          status = JSON::parse response
+          status = status['home_screen'].reduce(:merge)
+          puts status
+
+          def describe_mode mode
+            case mode.to_i
+            when -1
+              "not available"
+            when 0
+              "off"
+            when 1
+              "running"
+            when 3
+              "enabled, but not running"
+            else
+              "not available"
+            end
+          end
+
+          speech.unshift ["The pool temperature is #{status['pool_temp'].empty? ? 'unknown' : (status['pool_temp'] + ' degrees')}.",
+                          "The filter pump is #{describe_mode status['pool_pump']}.",
+                          "The solar panels are #{describe_mode status['solar_heater']}."].join "\n"
+
+        rescue Exception => e
+          speech.unshift "There was a problem accessing the pool controller.  The exception was #{e}"
+        end
       end
 
-      response.add_ssml('<speak>' + speech.join(' ') + '</speak>')
-      response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{request.name}" } )
+      alexa_response.add_ssml('<speak>' + speech.join(' ') + '</speak>')
+      alexa_response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{alexa_request.name}" } )
 
     when 'SESSION_ENDED_REQUEST'
       # Wrap up whatever we need to do.
-      # p "type:   #{request.type}"
-      # p "reason: #{request.reason}"
+      # p "type:   #{alexa_request.type}"
+      # p "reason: #{alexa_request.reason}"
       halt 200
     end
 
-    response.build_response end_session
+    alexa_response.build_response end_session
   end
 end
 
