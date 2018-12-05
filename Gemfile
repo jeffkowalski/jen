8ef95226f59669b8c3d87c9bf7752efd37e36942
Jeff Kowalski <jeff.kowalski@gmail.com>
2018-12-04 19:12:46 -0800

    Address a number of issues found by rubocop; improve README

--- Gemfile
+++ Gemfile
@@ -1,15 +1,15 @@
 source 'https://rubygems.org'
 
 gem 'alexa_rubykit'
-gem 'sinatra'
+gem 'chronic'
+gem 'dotiw'
+gem 'holidays'
 gem 'humanize'
 gem 'json'
 gem 'rest-client'
-gem 'holidays'
-gem 'chronic'
-gem 'dotiw'
+gem 'sinatra'
 
 # for robe:
+gem 'method_source', '>=0.8.2'
 gem 'pry'
 gem 'pry-doc', '>=0.6.0'
-gem 'method_source', '>=0.8.2'
--- jen.rb
+++ jen.rb
@@ -9,7 +9,7 @@ require 'webrick/https'
 require 'openssl'
 require 'alexa_rubykit'
 require 'json'
-
+require 'socket'
 require 'holidays'
 require 'chronic'
 require 'action_view'
@@ -17,79 +17,74 @@ include ActionView::Helpers::DateHelper
 
 class Date
   def weekend_or_holiday?
-    itself.saturday? or itself.sunday? or not Holidays.on(itself, :federal_reserve).empty?
+    itself.saturday? || itself.sunday? ||
+      !Holidays.on(itself, :federal_reserve).empty?
   end
 end
 
-
-
-def encodeURIcomponent str
-  return URI.escape(str, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+def encodeURIcomponent(str)
+  URI.escape(str, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 end
 
-
-def make_org_entry heading, context, priority, date, body
+def make_org_entry(heading, context, priority, date, body)
   #  $logger.debug "TODO [#{priority}] #{heading} #{context}"
   #  $logger.debug "SCHEDULED: #{date}"
   #  $logger.debug "#{body}"
-  title = encodeURIcomponent "[#{priority}] #{heading}  :#{context}:"
-  body  = encodeURIcomponent "SCHEDULED: #{date}\n#{body}"
-  uri = URI.parse("http://carbon:3333")
+  title = encode_uri_component "[#{priority}] #{heading}  :#{context}:"
+  body  = encode_uri_component "SCHEDULED: #{date}\n#{body}"
+  uri = URI.parse('http://carbon.zt:3333')
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Get.new("/capture/b/LINK/#{title}/#{body}")
   response = http.request(request)
-  return response
+  response
 end
-
 
 module AlexaRubykit
   class Response
     def add_ssml(speech_text)
-      @speech = { :type => 'SSML', :ssml => speech_text }
+      @speech = { type: 'SSML', ssml: speech_text }
       @speech
     end
   end
 end
 
-
 class Float
   require 'humanize'
 
-  def fuzz unit
+  def fuzz(unit)
     int = itself.to_int
     frac = ((itself - int) * 100).to_int
     prefix = case frac % 25
              when 0..3
-               "about"
+               'about'
              when 4..12
-               "a little more than"
+               'a little more than'
              when 13..22
-               "a little less than"
+               'a little less than'
              when 22..24
-               "about"
+               'about'
              end
     quadrant = case frac
                when 0..12
                  nil
-               when 13..12+25
+               when 13..(12 + 25)
                  'a quarter'
-               when 13+25..12+50
+               when (13 + 25)..(12 + 50)
                  'a half'
-               when 13+50..12+75
+               when (13 + 50)..(12 + 75)
                  'three quarters'
-               when 13+75..99
+               when (13 + 75)..99
                  int += 1
                  nil
                end
     composite = prefix
-    composite += " " + int.humanize.strip if quadrant.nil? || int != 0
-    quadrant = "and " + quadrant unless quadrant.nil? || int == 0
+    composite += ' ' + int.humanize.strip if quadrant.nil? || int != 0
+    quadrant = 'and ' + quadrant unless quadrant.nil? || int.zero?
     composite = [composite, quadrant, unit].compact.join(' ')
-    composite += 's' unless int == 1 && composite.match(/one\s*$/) || int == 0 && !quadrant.nil?
-    return composite
+    composite += 's' unless int == 1 && composite.match(/one\s*$/) || int.zero? && !quadrant.nil?
+    composite
   end
 end
-
 
 class MyServer < Sinatra::Base
   # We must return application/json as our content type.
@@ -97,7 +92,7 @@ class MyServer < Sinatra::Base
     content_type('application/json')
   end
 
-  #enable :sessions
+  # enable :sessions
   post '/' do
     # Check that it's a valid Alexa request
     request_json = JSON.parse(request.body.read.to_s)
@@ -108,8 +103,8 @@ class MyServer < Sinatra::Base
 
     # We can capture Session details inside of request.
     # See session object for more information.
-    session = request.session
-    p "--------------------------------------------------"
+    p '--------------------------------------------------'
+    # session = request.session
     # p "new?            #{session.new?}"
     # p "has_attributes? #{session.has_attributes?}"
     # p "session_id      #{session.session_id}"
@@ -120,8 +115,8 @@ class MyServer < Sinatra::Base
 
     # We can manipulate the request object.
     #
-    p "#{alexa_request.to_s}"
-    #p "#{alexa_request.request_id}"
+    p alexa_request.to_s
+    # p alexa_request.request_id
 
     end_session = true
 
@@ -130,8 +125,8 @@ class MyServer < Sinatra::Base
       # Process your Launch Request
       # Call your methods for your application here that process your Launch Request.
       # p "type: #{alexa_request.type}"
-      response.add_speech("Jen is listening")
-      response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Jen is listening!' } )
+      response.add_speech 'Jen is listening'
+      response.add_hash_card(title: 'Ruby Run', subtitle: 'Jen is listening!')
       end_session = false
 
     when 'INTENT_REQUEST'
@@ -139,7 +134,7 @@ class MyServer < Sinatra::Base
 
       case alexa_request.name
       when 'HelpIntent'
-        speech.unshift "You can ask me to remember something, or you can ask what are my jobs, or how much power are the solar panels producing, or how warm is the pool."
+        speech.unshift 'You can ask me to remember something, or you can ask what are my jobs, or how much power are the solar panels producing, or how warm is the pool.'
 
       when 'TaskIntent'
         require 'date'
@@ -150,12 +145,13 @@ class MyServer < Sinatra::Base
             date = Date.parse(alexa_request.slots['Date']['value'])
           rescue
             date = Time.now
-            speech.unshift "I didn't quite catch the date, so I put it down for today."
+            speech.unshift 'I didn\'t quite catch the date, so I put it down for today.'
           end
         end
 
-        org_response = make_org_entry alexa_request.slots['Message']['value'], '@home', '#C',
-                                      "<#{date.strftime('%F %a')}>", ""
+        org_response = make_org_entry alexa_request.slots['Message']['value'],
+                                      '@home', '#C',
+                                      "<#{date.strftime('%F %a')}>", ''
         if (org_response.code == '200')
           speech.unshift "Sure, I'll remember to #{alexa_request.slots['Message']['value']} on #{date.strftime('%A')} <say-as interpret-as='date'>????#{date.strftime('%m%d')}</say-as>."
         else
@@ -163,9 +159,9 @@ class MyServer < Sinatra::Base
         end
 
       when 'JobsIntent'
-        buffer = open("http://carbon:3333/agenda/day").read
-        #puts buffer
-        buffer.scan(/<span class="org-scheduled.*?"> \[#.\] (.*?)<\/span>/) { |job| speech.unshift "<p>#{job[0]}</p>"}
+        buffer = open('http://carbon.zt:3333/agenda/day').read
+        # puts buffer
+        buffer.scan(/<span class="org-scheduled.*?"> \[#.\] (.*?)<\/span>/) { |job| speech.unshift "<p>#{job[0]}</p>" }
 
       when 'SolarIntent'
         begin
@@ -173,8 +169,8 @@ class MyServer < Sinatra::Base
           require 'rest-client'
           require 'json'
 
-          sunpower_credentials = YAML.load_file File.join(Dir.home, '.credentials', "sunpower.yaml")
-          api_base_url = "https://monitor.us.sunpower.com/CustomerPortal"
+          sunpower_credentials = YAML.load_file File.join(Dir.home, '.credentials', 'sunpower.yaml')
+          api_base_url = 'https://monitor.us.sunpower.com/CustomerPortal'
 
           response = RestClient.post "#{api_base_url}/Auth/Auth.svc/Authenticate", sunpower_credentials.to_json, {'Content-Type' => 'application/json'}
           authorization = JSON.parse response
@@ -191,7 +187,7 @@ class MyServer < Sinatra::Base
               speech.unshift "The panels are producing #{production.fuzz('kilowatt')}."
             end
           else
-            speech.unshift "The panels aren't producing right now."
+            speech.unshift 'The panels aren\'t producing right now.'
           end
         rescue Exception => e
           speech.unshift "There was a problem accessing sunpower.  The exception was #{e}"
@@ -204,35 +200,35 @@ class MyServer < Sinatra::Base
           require 'json'
 
           api_key = 'EOOEMOW4YR6QNB07'
-          credentials = YAML.load_file File.join(Dir.home, '.credentials', "iaqualink.yaml")
+          credentials = YAML.load_file File.join(Dir.home, '.credentials', 'iaqualink.yaml')
 
           response = RestClient.post 'https://support.iaqualink.com/users/sign_in.json',
-                                     {api_key: api_key,
-                                      email: credentials[:username],
-                                      password: credentials[:password]}
-          session = JSON::parse response
+                                     api_key: api_key,
+                                     email: credentials[:username],
+                                     password: credentials[:password]
+          session = JSON.parse response
 
           response = RestClient.get 'https://iaqualink-api.realtime.io/v1/mobile/session.json',
-        	                    {params: {actionID: 'command',
-		                              command: 'get_home',
-		                              serial: credentials[:serial_number],
-		                              sessionID: session['session_id']}}
-          status = JSON::parse response
+                                    params: { actionID: 'command',
+                                              command: 'get_home',
+                                              serial: credentials[:serial_number],
+                                              sessionID: session['session_id'] }
+          status = JSON.parse response
           status = status['home_screen'].reduce(:merge)
           puts status
 
-          def describe_mode mode
+          def describe_mode(mode)
             case mode.to_i
             when -1
-              "not available"
+              'not available'
             when 0
-              "off"
+              'off'
             when 1
-              "running"
+              'running'
             when 3
-              "enabled, but not running"
+              'enabled, but not running'
             else
-              "not available"
+              'not available'
             end
           end
 
@@ -240,61 +236,60 @@ class MyServer < Sinatra::Base
                           "The filter pump is #{describe_mode status['pool_pump']}.",
                           "The solar panels are #{describe_mode status['solar_heater']}."].join "\n"
 
-        rescue Exception => e
+        rescue StandardError => e
           speech.unshift "There was a problem accessing the pool controller.  The exception was #{e}"
         end
 
       when 'PowerIntent'
         begin
-
-          today = Date::today
+          today = Date.today
           tomorrow = today + 1
-          hour = Time::now.hour
+          hour = Time.now.hour
 
           if today.weekend_or_holiday?
             # weekend
             if hour < 15
-              # [:off_peak, "3pm", :peak]
-              change = "3pm"
-              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
+              # [:off_peak, '3pm', :peak]
+              change = '3pm'
+              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
             elsif hour < 19
-              # [:peak, "7pm", :off_peak]
-              change = "7pm"
-              speech.unshift "Now is a really expensive time to use electricity, because it's peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the off-peak rate."
+              # [:peak, '7pm', :off_peak]
+              change = '7pm'
+              speech.unshift "Now is a really expensive time to use electricity, because it's peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the off-peak rate."
             elsif tomorrow.weekend_or_holiday?
-              # [:off_peak, "tomorrow 3pm", :peak]
-              change = "tomorrow 3pm"
-              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
+              # [:off_peak, 'tomorrow 3pm', :peak]
+              change = 'tomorrow 3pm'
+              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
             else
-              # [:off_peak, "tomorrow 7am", :partial_peak]
-              change = "tomorrow 7am"
-              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
+              # [:off_peak, 'tomorrow 7am', :partial_peak]
+              change = 'tomorrow 7am'
+              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
             end
           else
             if hour < 7
-              # [:off_peak, "7am", :partial_peak]
-              change = "7am"
-              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
+              # [:off_peak, '7am', :partial_peak]
+              change = '7am'
+              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
             elsif hour < 14
-              # [:partial_peak, "2pm", :peak]
-              change = "2pm"
-              speech.unshift "Now is a not the best time to use electricity, because it's partial-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}."
+              # [:partial_peak, '2pm', :peak]
+              change = '2pm'
+              speech.unshift "Now is a not the best time to use electricity, because it's partial-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}."
             elsif hour < 21
-              # [:peak, "9pm", :partial_peak]
-              change = "9pm"
-              speech.unshift "Now is a really expensive time to use electricity, because it's peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the partial-peak rate."
+              # [:peak, '9pm', :partial_peak]
+              change = '9pm'
+              speech.unshift "Now is a really expensive time to use electricity, because it's peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the partial-peak rate."
             elsif hour < 23
-              # [:partial_peak, "11pm", :off_peak]
-              change = "11pm"
-              speech.unshift "Now is a not the best time to use electricity, because it's partial-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change} when it will change to the off-peak rate."
+              # [:partial_peak, '11pm', :off_peak]
+              change = '11pm'
+              speech.unshift "Now is a not the best time to use electricity, because it's partial-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change} when it will change to the off-peak rate."
             elsif tomorrow.weekend_or_holiday?
-              # [:off_peak, "tomorrow 3pm", :peak]
-              change = "tomorrow 3pm"
-              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
+              # [:off_peak, 'tomorrow 3pm', :peak]
+              change = 'tomorrow 3pm'
+              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
             else
-              # [:off_peak, "tomorrow 7am", :partial_peak]
-              change = "tomorrow 7am"
-              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time::now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
+              # [:off_peak, 'tomorrow 7am', :partial_peak]
+              change = 'tomorrow 7am'
+              speech.unshift "Now is a great time to use electricity, because it's off-peak rate for #{distance_of_time_in_words(Time.now, Chronic.parse(change))} until #{change}, when it will change to the peak rate."
             end
           end
 
@@ -306,7 +301,7 @@ class MyServer < Sinatra::Base
       $logger.info speech
 
       alexa_response.add_ssml('<speak>' + speech.join(' ') + '</speak>')
-      alexa_response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{alexa_request.name}" } )
+      alexa_response.add_hash_card(title: 'Ruby Intent', subtitle: "Intent #{alexa_request.name}")
 
     when 'SESSION_ENDED_REQUEST'
       # Wrap up whatever we need to do.
@@ -319,32 +314,31 @@ class MyServer < Sinatra::Base
   end
 end
 
-
-CERT_PATH = './'
+CERT_PATH = './'.freeze
 
 webrick_options = {
-  :Host               => '192.168.1.34',
-  :Port               => 8443,
-  :Logger             => WEBrick::Log::new($stderr, WEBrick::Log::DEBUG),
-  :DocumentRoot       => "/ruby/htdocs",
-  :SSLEnable          => true,
-  :SSLVerifyClient    => OpenSSL::SSL::VERIFY_NONE,
-  :SSLCertificate     => OpenSSL::X509::Certificate.new(  File.open(File.join(CERT_PATH, "certificate.pem")).read),
-  :SSLPrivateKey      => OpenSSL::PKey::RSA.new(          File.open(File.join(CERT_PATH, "private-key.pem")).read),
-  :app                => MyServer
+  Host:            Socket.ip_address_list[1].ip_address,
+  Port:            8443,
+  Logger:          WEBrick::Log::new($stderr, WEBrick::Log::DEBUG),
+  DocumentRoot:    '/ruby/htdocs',
+  SSLEnable:       true,
+  SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE,
+  SSLCertificate:  OpenSSL::X509::Certificate.new(File.open(File.join(CERT_PATH, 'certificate.pem')).read),
+  SSLPrivateKey:   OpenSSL::PKey::RSA.new(File.open(File.join(CERT_PATH, 'private-key.pem')).read),
+  app:             MyServer
 }
-
 
 require 'logger'
 
 LOGFILE = File.join(Dir.home, '.log', 'jen.log')
+LOGFILE = 'STDOUT'
 
 def redirect_output
   unless LOGFILE == 'STDOUT'
     logfile = File.expand_path(LOGFILE)
-    FileUtils.mkdir_p(File.dirname(logfile), :mode => 0755)
+    FileUtils.mkdir_p(File.dirname(logfile), mode: 0o755)
     FileUtils.touch logfile
-    File.chmod 0644, logfile
+    File.chmod 0o644, logfile
     $stdout.reopen logfile, 'a'
   end
   $stderr.reopen $stdout
@@ -360,6 +354,5 @@ def setup_logger
 end
 
 setup_logger
-
 
 Rack::Server.start webrick_options
